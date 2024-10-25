4849a8c2bd828a582544ba38e7d6f1e7afea9fbf
Ross-dch97 <dchross97@outlook.it>
2024-10-25 23:56:11 +0200

    Selfwork-variabili

---
+++ main.js
@@ -0,0 +1,28 @@
+
+let num1 = 10;
+
+let num2 = 15;
+
+console.log( num1 );
+
+console.log( num2 );
+
+console.log( num1 + num2 );
+
+
+let stringa = 'ciao';
+
+console.log( stringa );
+
+stringa = 'buongiorno'
+
+console.log( stringa );
+
+const PIGREGO = 3.14159265359;
+
+console.log( PIGRECO );
+
+PIGRECO = 5.14 ;
+
+console.log( PIGRECO );
+
